115c26e0f44162f6e99e6f9878622bd0870d50b6
Dean _Gaffney <gaffmasterflex123@gmail.com>
2015-11-17 21:37:29 +0000

    Created a stream that saves users

---
+++ src/controllers/Main.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import java.io.File;
+
+import utils.Serializer;
+import utils.XMLSerializer;
+import edu.princeton.cs.introcs.In;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception 
+	{
+		File usersFile = new File("data/users.dat");
+		Serializer serializer = new XMLSerializer(usersFile);
+		MovieRecommenderAPI movieRecommender = new MovieRecommenderAPI(serializer);
+
+		In inUsers = new In(usersFile);
+
+		//each field is separated(delimited) by a '|'
+		String delims = "[|]";
+		while (!inUsers.isEmpty()) 
+		{
+			// get user and rating from data source
+			String userDetails = inUsers.readLine();
+
+			// parse user details string
+			String[] userTokens = userDetails.split(delims);
+			movieRecommender.createUser(Long.parseLong(userTokens[0]),userTokens[1],userTokens[2],Integer.parseInt(userTokens[3]),userTokens[4],userTokens[5]);
+			// output user data to console.
+			if (movieRecommender.getUsers()!=null) 
+			{
+				System.out.println(movieRecommender.getUsers().size());
+			}
+			else
+			{
+				throw new Exception("Invalid member length: "+userTokens.length);
+			}
+
+		}
+		for(int i = 0;i<=movieRecommender.getUsers().size();i++)
+		{
+			System.out.println(movieRecommender.getUser(Long.valueOf(i)));
+		}
+		movieRecommender.store();
+	}
+}
+
---
+++ src/controllers/MovieRecommenderAPI.java
@@ -0,0 +1,71 @@
+package controllers;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.base.Optional;
+
+import models.User;
+import utils.Serializer;
+
+public class MovieRecommenderAPI 
+{
+	private Serializer serializer;
+	
+	private Map<Long,   User>   userIndex       = new HashMap<>();
+	private Map<String, User>   emailIndex      = new HashMap<>();
+	//private Map<Long, Activity> activitiesIndex = new HashMap<>();
+
+	public MovieRecommenderAPI()
+	{}
+
+	public MovieRecommenderAPI(Serializer serializer)
+	{
+		this.serializer = serializer;
+	}
+	 @SuppressWarnings("unchecked")
+	  public void load() throws Exception
+	  {
+	    serializer.read();
+	    userIndex = (Map<Long, User>)     serializer.pop();
+	  }
+	  
+	  public void store() throws Exception
+	  {
+	    serializer.push(userIndex);
+	    serializer.write(); 
+	  }
+	  
+	  public Collection<User> getUsers ()
+	  {
+	    return userIndex.values();
+	  }
+	  
+	  public  void deleteUsers() 
+	  {
+	    userIndex.clear();
+	  }
+	  
+	  public User createUser(Long id,String firstName, String lastName,int age,String gender,String occupation) 
+	  {
+	    User user = new User (id,firstName, lastName,age,gender,occupation);
+	    userIndex.put(user.id, user);
+	    return user;
+	  }
+	  
+	  public User getUserByEmail(String email) 
+	  {
+	    return emailIndex.get(email);
+	  }
+
+	  public User getUser(Long id) 
+	  {
+	    return userIndex.get(id);
+	  }
+
+	  public void deleteUser(Long id) 
+	  {
+	    User user = userIndex.remove(id);
+	  }
+}
